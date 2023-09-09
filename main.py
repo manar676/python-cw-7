79c4c68159b7220fb3b5e8953a8175dd017827a5
manar676 <142670547+manar676@users.noreply.github.com>
2023-09-10 02:35:12 +0300

    Create main.py
    done

---
+++ main.py
@@ -0,0 +1,32 @@
+class person :
+    # name="manar"
+    # age=17 
+    def is_adult(self):
+       if self.age > 18 :
+          print("You have too much responsibilities")
+       else:
+          print("lucky")
+          
+first_person=person()
+print(first_person.name)
+print(first_person.age)
+first_person.is_adult()
+class person :
+    def __init__(self,name,age):
+       self.name= name
+       self.age=age 
+
+    def is_adult(self):
+       if self.age > 18 :
+          print("You have too much responsibilities")
+       else:
+          print("lucky")
+    def __str__(self):
+          return f"my name is{self.name} and I am {self.age}years old)"
+    
+
+first_person=person("manar",17)
+print(first_person.name)
+print(first_person.age)
+first_person.is_adult()
+print(first_person)
